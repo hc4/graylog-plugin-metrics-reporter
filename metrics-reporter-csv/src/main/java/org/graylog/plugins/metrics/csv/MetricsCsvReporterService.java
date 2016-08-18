bd3251b9c0030c3fd05c652648883a28615caacf
Lennart Koopmann <lennart@graylog.com>
2016-08-17 21:37:07 -0700

    omg license headers #1

--- metrics-reporter-csv/src/main/java/org/graylog/plugins/metrics/csv/MetricsCsvReporterService.java
+++ metrics-reporter-csv/src/main/java/org/graylog/plugins/metrics/csv/MetricsCsvReporterService.java
@@ -1,5 +1,5 @@
 /**
- * This file is part of Graylog Metrics CSV Reporter Plugin.
+ * This file is part of Graylog Metrics shared code.
  *
  * Graylog Metrics CSV Reporter Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
